b043ac01ec4934b7911e4ae4d3b55ebade6a4b4e
okänd <git@okand.email>
2026-09-09 14:43:55 +0200

    spell desktop correctly

--- Casks/okand-webos-dev-manager-desktop.rb
+++ Casks/okand-webos-dev-manager-desktop.rb
@@ -6,7 +6,7 @@ cask "okand-webos-dev-manager-desktop" do
   version "1.99.20"
   sha256 "e4a85cbf0922e0832d9f7bef0c130c914979ac499124e367b23960f9595e8f0d"
 
-  url "https://github.com/webosbrew/dev-manager-desktop/releases/download/v#{version}/webOS.Dev.Manager_#{version}_universal.dmg"
+  url "https://github.com/webosbrew/dev-manager-desktop/releases/download/v#{version}/webOS.Dev.Manager_#{version}_universal.dmg",
       verified: "github.com/webosbrew/dev-manager-desktop/releases/download/"
   name "webOS Dev Manager Desktop"
   desc "Device/DevMode Manager for webOS TV"
